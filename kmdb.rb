961481fbfc86c04a5aad1cd14936f1dd0d16df3b
chaitanyaratakondla <chaitanya.ratakondla@kellogg.northwestern.edu>
2022-02-08 05:52:47 +0000

    Completed

--- kmdb.rb
+++ kmdb.rb
@@ -297,6 +297,11 @@ puts ""
 # Query the movies data and loop through the results to display the movies output
 # TODO!
 
+movies = Movie.all
+for movie in movies
+    puts "#{movie.title} #{movie.year_of_release} #{movie.rating} #{movie.person.name}"
+end
+
 # Prints a header for the cast output
 puts ""
 puts "Top Cast"
@@ -305,3 +310,8 @@ puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie
 # TODO!
+
+roles = Role.all
+for role in roles
+    puts "#{role.movie.title} #{role.person.name} #{role.character_name}"
+end
